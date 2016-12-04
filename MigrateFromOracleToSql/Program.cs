600aae2a85bc2b63d1b96d328463b1df8bbe09d1
Anton Skripec <anton.skripec@sigma.se>
2016-12-04 16:45:04 +0100

    code

--- MigrateFromOracleToSql/Program.cs
+++ MigrateFromOracleToSql/Program.cs
@@ -12,6 +12,70 @@ namespace MigrateFromOracleToSql
             LoadData();
         }
 
+        private static Fields CreateErrandFields()
+        {
+            var returnFields = new Fields();
+
+            returnFields.Add(new FieldInfo
+            {
+                OrdinalPosition = 0,
+                Name = "Id",
+                Type = typeof(int)
+            });
+
+            returnFields.Add(new FieldInfo
+            {
+                OrdinalPosition = 1,
+                Name = "ResponsibleManagerId",
+                Type = typeof(int)
+            });
+
+            returnFields.Add(new FieldInfo
+            {
+                OrdinalPosition = 2,
+                Name = "ErrandTypeId",
+                Type = typeof(int)
+            });
+
+            returnFields.Add(new FieldInfo
+            {
+                OrdinalPosition = 3,
+                Name = "Comment",
+                Type = typeof(string)
+            });
+
+            returnFields.Add(new FieldInfo
+            {
+                OrdinalPosition = 4,
+                Name = "Created",
+                Type = typeof(DateTime)
+            });
+
+            returnFields.Add(new FieldInfo
+            {
+                OrdinalPosition = 5,
+                Name = "CurrentPhaseId",
+                Type = typeof(int)
+            });
+
+            returnFields.Add(new FieldInfo
+            {
+                OrdinalPosition = 7,
+                Name = "EventDescription",
+                Type = typeof(string)
+            });
+
+            returnFields.Add(new FieldInfo
+            {
+                OrdinalPosition = 8,
+                Name = "EstimatedEndDate",
+                Type = typeof(DateTime?)
+            });
+
+            return returnFields;
+        }
+
+
         private static Fields CreateErrandTypeFields()
         {
             var returnFields = new Fields();
@@ -187,45 +251,44 @@ namespace MigrateFromOracleToSql
 
             var bulkCopyDataIntoSqlServer = new BulkCopyDataIntoSqlServer();
 
-            //Users.
-            LoadUser(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameFullnameTimestampFields());
+            ////Permissions.
+            //LoadPermissions(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameFields());
 
-            //Approval procedures.
-            LoadApprovalProcedures(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
+            ////Users.
+            //LoadUser(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameFullnameTimestampFields());
 
-            //Distributors.
-            LoadDistributors(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
+            ////Approval procedures.
+            //LoadApprovalProcedures(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
 
-            //Dosage forms.
-            LoadDosageForms(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
+            ////Distributors.
+            //LoadDistributors(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
 
-            //Manufacturers.
-            LoadManufacturers(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
+            ////Dosage forms.
+            //LoadDosageForms(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
 
-            //Open Storage.
-            LoadOpenStorage(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
+            ////Manufacturers.
+            //LoadManufacturers(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
 
-            //Package type.
-            LoadPackageType(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
+            ////Open Storage.
+            //LoadOpenStorage(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
 
-            //Package detail type.
-            LoadPackageDetailType(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
+            ////Package type.
+            //LoadPackageType(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
 
-            //Permissions.
-            LoadPermissions(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameFields());
+            ////Package detail type.
+            //LoadPackageDetailType(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
 
-            //Responsible managers.
-            LoadResponsibleManagers(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
+            ////Responsible managers.
+            //LoadResponsibleManagers(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
 
-            //Errand type groups and Errand types.
-            LoadErrandTypeGroups(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
-            LoadErrandTypes(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateErrandTypeFields());
+            ////Errand type groups and Errand types.
+            //LoadErrandTypeGroups(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
+            //LoadErrandTypes(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateErrandTypeFields());
 
-
-            //Phase groups and phases.
-            LoadPhaseGroups(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
-            LoadPhases(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreatePhasesFields());
-
+            ////Phase groups and phases.
+            //LoadPhaseGroups(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateIdNameTimestampFields());
+            //LoadPhases(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreatePhasesFields());
+            LoadErrands(bulkCopyDataIntoSqlServer, directoryInfo, userName, CreateErrandFields());
 
 
             Console.WriteLine("Finished loading data");
@@ -346,9 +409,9 @@ namespace MigrateFromOracleToSql
         }
 
         private static void LoadPhaseGroups(BulkCopyDataIntoSqlServer bulkCopyDataIntoSqlServer,
-            DirectoryInfo directoryInfo,
-            string userName,
-            Fields fields)
+                                            DirectoryInfo directoryInfo,
+                                            string userName,
+                                            Fields fields)
         {
             Console.WriteLine("Start loading PhaseGroups.");
 
@@ -362,35 +425,51 @@ namespace MigrateFromOracleToSql
         }
 
         private static void LoadErrandTypeGroups(BulkCopyDataIntoSqlServer bulkCopyDataIntoSqlServer,
-                                            DirectoryInfo directoryInfo,
-                                            string userName,
-                                            Fields fields)
+                                                DirectoryInfo directoryInfo,
+                                                string userName,
+                                                Fields fields)
         {
             Console.WriteLine("Start loading Errand type groups.");
 
             bulkCopyDataIntoSqlServer.LoadTable(fields,
-                "ErrandTypeGroups",
-                Settings.Default.ConnectionString,
-                Path.Combine(directoryInfo.FullName, "ERRAND_TYPE_GROUP.XLSX"),
-                userName);
+                                                "ErrandTypeGroups",
+                                                Settings.Default.ConnectionString,
+                                                Path.Combine(directoryInfo.FullName, "ERRAND_TYPE_GROUP.XLSX"),
+                                                userName);
 
             Console.WriteLine("Finished loading Errand type groups.");
         }
 
         private static void LoadManufacturers(BulkCopyDataIntoSqlServer bulkCopyDataIntoSqlServer,
-                                            DirectoryInfo directoryInfo,
-                                            string userName,
-                                            Fields fields)
+                                                DirectoryInfo directoryInfo,
+                                                string userName,
+                                                Fields fields)
         {
             Console.WriteLine("Start loading Manufacturers.");
 
             bulkCopyDataIntoSqlServer.LoadTable(fields,
-                "Manufacturers",
-                Settings.Default.ConnectionString,
-                Path.Combine(directoryInfo.FullName, "MANUFACTURER.XLSX"),
-                userName);
+                                                "Manufacturers",
+                                                Settings.Default.ConnectionString,
+                                                Path.Combine(directoryInfo.FullName, "MANUFACTURER.XLSX"),
+                                                userName);
 
             Console.WriteLine("Finished loading Manufacturers.");
+        }
+
+        private static void LoadErrands(BulkCopyDataIntoSqlServer bulkCopyDataIntoSqlServer,
+                                        DirectoryInfo directoryInfo,
+                                        string userName,
+                                        Fields fields)
+        {
+            Console.WriteLine("Start loading Errands.");
+
+            bulkCopyDataIntoSqlServer.LoadTable(fields,
+                                                "Errands",
+                                                Settings.Default.ConnectionString,
+                                                Path.Combine(directoryInfo.FullName, "ERRAND.XLSX"),
+                                                userName);
+
+            Console.WriteLine("Finished loading Errands.");
         }
 
         private static void LoadErrandTypes(BulkCopyDataIntoSqlServer bulkCopyDataIntoSqlServer,
@@ -401,34 +480,34 @@ namespace MigrateFromOracleToSql
             Console.WriteLine("Start loading Errand types.");
 
             bulkCopyDataIntoSqlServer.LoadTable(fields,
-                "ErrandTypes",
-                Settings.Default.ConnectionString,
-                Path.Combine(directoryInfo.FullName, "ERRAND_TYPE.XLSX"),
-                userName);
+                                                "ErrandTypes",
+                                                Settings.Default.ConnectionString,
+                                                Path.Combine(directoryInfo.FullName, "ERRAND_TYPE.XLSX"),
+                                                userName);
 
             Console.WriteLine("Finished loading Errand types.");
         }
 
         private static void LoadDosageForms(BulkCopyDataIntoSqlServer bulkCopyDataIntoSqlServer,
-            DirectoryInfo directoryInfo,
-            string userName,
-            Fields fields)
+                                            DirectoryInfo directoryInfo,
+                                            string userName,
+                                            Fields fields)
         {
             Console.WriteLine("Start loading DosageForms.");
 
             bulkCopyDataIntoSqlServer.LoadTable(fields,
-                "DosageForms",
-                Settings.Default.ConnectionString,
-                Path.Combine(directoryInfo.FullName, "DOSAGE_FORM.XLSX"),
-                userName);
+                                                "DosageForms",
+                                                Settings.Default.ConnectionString,
+                                                Path.Combine(directoryInfo.FullName, "DOSAGE_FORM.XLSX"),
+                                                userName);
 
             Console.WriteLine("Finished loading DosageForms.");
         }
 
         private static void LoadDistributors(BulkCopyDataIntoSqlServer bulkCopyDataIntoSqlServer,
-            DirectoryInfo directoryInfo,
-            string userName,
-            Fields fields)
+                                            DirectoryInfo directoryInfo,
+                                            string userName,
+                                            Fields fields)
         {
             Console.WriteLine("Start loading Distributors.");
 
